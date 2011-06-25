f055afbc38590983b147f01f16126f323b66c753
August <august@alum.rpi.edu>
2011-06-25 01:27:44 -0400

    FML

--- db/migrate/20110625050043_private_feeds_and_such.rb
+++ db/migrate/20110625050043_private_feeds_and_such.rb
@@ -2,7 +2,7 @@ class PrivateFeedsAndSuch < ActiveRecord::Migration
   def up
     change_table :feeds do |t|
       t.remove :is_public
-      t.remove :is_read_ony
+      t.remove :is_read_only
       t.boolean :is_viewable, :default => 1, :null => false
       t.boolean :is_submittable, :default => 1, :null => false
     end
