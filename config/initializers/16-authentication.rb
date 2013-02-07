91eec9c315e91260775c2ab498cd1af56235eb6e
Kenley Cheung <ken@lordkenthegreat.com>
2013-02-07 11:24:08 -0500

    Add SSO configuration.

---
+++ config/initializers/16-authentication.rb
@@ -0,0 +1,22 @@
+# OmniAuth configuration for external authentication
+
+# Uncomment this section if external autentication is being used.
+
+#require "omniauth"
+
+# OmniAuth logging configuration.  Defaults to STDOUT
+
+#OmniAuth.config.logger = Rails.logger
+
+# Central Authentication System (CAS) configuration
+# OmniAuth CAS requires at least one of the following two configuration options:
+#
+#    url - Defines the URL of your CAS server (i.e. https://cas-auth.rpi.edu/cas)
+#    host - Defines the host of your CAS server. Optional if using url
+#    login_url - Defines the URL used to prompt users for their login information.
+#                Defaults to /login If no host is configured, the host application's domain will be used.
+#
+# For additional configuration options and information, read https://github.com/dlindahl/omniauth-cas
+
+#require "omniauth-cas"
+#config.omniauth :cas, url: 'https://cas-auth.example.com/'
